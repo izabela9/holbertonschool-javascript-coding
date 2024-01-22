977c4ef4c769caf6e0d3f5d125badfbb960db038
izabela9 <isabelanelaj1@gmail.com>
2024-01-22 07:07:29 -0800

    write a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const fileData = process.argv[3];
 
-fs.writeFile(filePath,fileData, 'utf-8', (err) => {
+fs.writeFile(filePath, fileData, 'utf-8', (err) => {
   if (err) throw err;
-}); 
+});
