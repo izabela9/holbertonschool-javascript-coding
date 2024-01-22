86f6ec082851e1c8dc50ca5f59beb736524ab21d
izabela9 <isabelanelaj1@gmail.com>
2024-01-22 07:56:39 -0800

    status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+
+request.get(process.argv[2], function (error, response, body) {
+  if (!error & response.statusCode) {
+    console.log('code: ', response.statusCode);
+  }
+}
+);
