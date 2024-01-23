1b38181470e970fd4b869659dd2772553dada3a6
izabela9 <isabelanelaj1@gmail.com>
2024-01-23 05:16:18 -0800

    number of films by a character

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
 
-request(process.argv[2], function(error, response, body) => {
+request(process.argv[2], (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else {
-    const filmsData = JSON.parse(response.body);
-    const newData = filmsData.results.filter((film) => {
-      return film.characters.includes('https://swapi-api.hbtn.io/api/people/18/');
-    });
-    console.log(newData.length);
+    console.log(error);
   }
+  const filmsData = JSON.parse(body);
+  const newData = filmsData.results.filter((film) => {
+    return film.characters.includes('https://swapi-api.hbtn.io/api/people/18/');
+  });
+  console.log(newData.length);
 });
